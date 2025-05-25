00f7d9fe449d53e47334340c05937783d024f591
Jannis Dommer <jannis.dommer@outlook.de>
2025-05-25 15:50:21 +0200

    style(MatchMate): adjust css

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -8,8 +7,6 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/groups">Groups</RouterLink>
@@ -18,7 +15,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </div>
   </header>
 
-  <RouterView />
+  <RouterView class="centered"/>
 </template>
 
 <style scoped>
@@ -48,13 +45,16 @@ nav a.router-link-exact-active:hover {
 }
 
 nav a {
-  display: inline-block;
   padding: 0 1rem;
   border-left: 1px solid var(--color-border);
 }
 
 nav a:first-of-type {
   border: 0;
+}
+
+.centered {
+  margin: 0 auto;
 }
 
 @media (min-width: 1024px) {
