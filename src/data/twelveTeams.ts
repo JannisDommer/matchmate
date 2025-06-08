630b42e1dee12d55b114c5bd8dfe93e9f00f5757
Jannis Dommer <jannis.dommer@outlook.de>
2025-06-08 12:29:50 +0200

    scoring

--- src/data/twelveTeams.ts
+++ src/data/twelveTeams.ts
@@ -99,8 +99,8 @@ export const twelveTeamsMatches: Match[] = [
         }
       ]
     },
-    "scoreHome": 0,
-    "scoreAway": 0
+    "scoreHome": 2,
+    "scoreAway": 2
   },
   {
     "group": "Group C",
